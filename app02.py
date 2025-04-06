fc51f93aa39d194f79d4be531aee6dc231446f67
mildcharinrat <beamind.charin@gmail.com>
2025-04-06 20:06:55 +0700

    Update app02.py

--- app02.py
+++ app02.py
@@ -3,7 +3,7 @@ import pandas as pd
 import google.generativeai as genai
 
 st.set_page_config(page_title="🤖 Chat with Your Data", layout="wide")
-st.title("💬 Chat with Your Data – AI-Powered Insights, Driven by Gemini 🤖")
+st.title("💬 Chat with Your Data | AI-Powered Insights, Driven by Gemini 🤖")
 st.caption("Upload your dataset and data dictionary, then ask questions — Gemini will will give natural language insights!")
 
 # -------- Gemini Config -------- #
